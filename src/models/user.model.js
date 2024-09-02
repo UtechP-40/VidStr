7a62bec9bad305d8735d46350513d946a2ebad72
UtechP-40 <pradeep2420pradeep@gmail.com>
2024-09-02 12:25:28 +0530

    JWT refresh and access tokens added andencrypted password

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose, {Schema} from "mongoose";
-
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 const userSchema = new Schema(
     {
         user:{
@@ -17,7 +18,7 @@ const userSchema = new Schema(
             trim:true,
            lowercase:true,
         },
-        fullname:{
+        fullName:{
             type:String,
             required:true,
             // unique:true,
@@ -50,6 +51,39 @@ const userSchema = new Schema(
         // default:null
     }},{timestamps:true}
 )
+
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next()
+    this.password = await bcrypt.hash(this.password,10)
+    next()
+})
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt .sign(
+        {
+            _id:this._id,
+            email:this.email,
+            fullName:this.fullName,
+            username:this.username
+    },
+    process.env.ACCESS_TOKEN_SECRATE,
+    ),
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt .sign(
+        {
+            _id:this._id
+    },
+    process.env.REFRESH_TOKEN_SECRATE,
+    ),
+    {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+}
 
 
 export const User = mongoose.model("User",userSchema)
