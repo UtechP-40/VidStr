7df7ebd60313142f0223c33b0b1a8d0a26ac1963
UtechP-40 <pradeep2420pradeep@gmail.com>
2024-10-25 23:48:27 +0530

    BUG fixed and LOGIN and logout APIs added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser,refreshAccessToken } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middlewares.js";
-
+// import 
+import { verifyJWT } from './../middlewares/auth.middleware.js';
 
 const router = Router();
 
@@ -21,4 +22,9 @@ router.route("/register").post(
     registerUser
     )
 
+    router.route("/login").post(loginUser)
+
+
+    router.route("/logout").post(verifyJWT,logoutUser)
+    router.route("/refresh-token").post(refreshAccessToken)
 export default router
