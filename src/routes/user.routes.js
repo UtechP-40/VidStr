57563e8e74d49174378fc85c7d23168c2518bb42
UtechP-40 <pradeep2420pradeep@gmail.com>
2025-02-17 01:21:35 +0530

    Base Structure

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,22 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser,refreshAccessToken } from "../controllers/user.controller.js";
-import { upload } from "../middlewares/multer.middlewares.js";
-// import 
-import { verifyJWT } from './../middlewares/auth.middleware.js';
+import { 
+    loginUser, 
+    logoutUser, 
+    registerUser, 
+    refreshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateUserAvatar, 
+    updateUserCoverImage, 
+    getUserChannelProfile, 
+    getWatchHistory, 
+    updateAccountDetails
+} from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middlewares.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-const router = Router();
 
-// router.post("/register", registerUser);
+const router = Router()
 
 router.route("/register").post(
     upload.fields([
@@ -22,9 +32,19 @@ router.route("/register").post(
     registerUser
     )
 
-    router.route("/login").post(loginUser)
+router.route("/login").post(loginUser)
 
+//secured routes
+router.route("/logout").post(verifyJWT,  logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 
-    router.route("/logout").post(verifyJWT,logoutUser)
-    router.route("/refresh-token").post(refreshAccessToken)
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
+
 export default router
